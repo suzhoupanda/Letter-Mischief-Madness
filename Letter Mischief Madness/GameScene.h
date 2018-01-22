060a66b69dd3af84f94b6e6090c12a12e44189d9
yongrenwang <czie.alexmak@gmail.com>
2018-01-22 14:42:44 +0800

    Initial Commit

---
+++ Letter Mischief Madness/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Letter Mischief Madness
+//
+//  Created by Aleksander Makedonski on 1/22/18.
+//  Copyright © 2018 Aleksander Makedonski. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ Letter Mischief Madness/GameViewController.h
@@ -0,0 +1,15 @@
+//
+//  GameViewController.h
+//  Letter Mischief Madness
+//
+//  Created by Aleksander Makedonski on 1/22/18.
+//  Copyright © 2018 Aleksander Makedonski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+#import <GameplayKit/GameplayKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
