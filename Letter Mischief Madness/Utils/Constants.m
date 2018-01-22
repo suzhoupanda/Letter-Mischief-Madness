8bc4cc42329cde8c7e29254aa43b414bf3c6259e
yongrenwang <czie.alexmak@gmail.com>
2018-01-22 20:17:05 +0800

    First Commit

---
+++ Letter Mischief Madness/Utils/Constants.m
@@ -0,0 +1,26 @@
+//
+//  Constants.m
+//  Letter Mischief Madness
+//
+//  Created by Aleksander Makedonski on 1/22/18.
+//  Copyright © 2018 Aleksander Makedonski. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "Constants.h"
+
+
+ CGFloat kDebugSpawnAreaWidth = 600.0;
+ CGFloat kDebugSpawnAreaHeight = 600.0;
+ CGFloat kDebugSpawnArea_UpperY = 300.0;
+ CGFloat kDebugSpawnArea_LowerY = -300.0;
+ CGFloat kDebugSpawnArea_LowerX = -250.0;
+ CGFloat kDebugSpawnArea_UpperX = 250.0;
+ int kNumberOfOnScreenDebugPoints = 5;
+int kNumberOfClouds = 9;
+
+ CGFloat kZPositionCloud = 5;
+ CGFloat kZPositionLetter = 3;
+
+
